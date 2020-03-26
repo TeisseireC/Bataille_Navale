6a37fa2fe9ff5cbbdf3e0988cd3c6c87f3e55b90
Dimitri <dimitri.dg23@gmail.com>
2020-03-26 11:11:20 +0100

    Bateau v1
    bateau v1

--- src/bateau/Bateau.java
+++ src/bateau/Bateau.java
@@ -1,4 +1,32 @@
 package bateau;
 
 public class Bateau {
+
+    private int[] startPosition;
+    private int modele;
+    private String direction;
+
+    public void setStartPosition(int[] startPosition) {
+        this.startPosition = startPosition;
+    }
+
+    public void setModele(int modele) {
+        this.modele = modele;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public int[] getStartPosition() {
+        return startPosition;
+    }
+
+    public int getModele() {
+        return modele;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
